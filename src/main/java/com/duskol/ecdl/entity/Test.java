8ec8eb206232e3bb7e18bf8a8d2d4842cf107149
duskol <dulleo@gmail.com>
2018-10-30 00:48:43 +0100

    initial commit

--- src/main/java/com/duskol/ecdl/entity/Test.java
+++ src/main/java/com/duskol/ecdl/entity/Test.java
@@ -1,4 +1,37 @@
 package com.duskol.ecdl.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Test {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
+    private long id;
+
+    @NotBlank
+    @Column(name="name")
+    private String name;
+
+    @NotBlank
+    @Column(name="active")
+    private Boolean active;
+
+    @NotBlank
+    @Column(name="duration")
+    private int duration;
+
+    @NotBlank
+    @Column(name="min_passing_score")
+    private int minPassingScore;
+
+    @NotBlank
+    @Column(name="bidirectional")
+    private Boolean bidirectional;
+
+    @ManyToOne
+    @JoinColumn(name="id")
+    private AppUser user;
 }
