f75dff235117ef109b5db1d6c210fb13430d158a
duskol <dulleo@gmail.com>
2018-10-30 00:11:21 +0100

    Initial commit

---
+++ src/main/java/com/duskol/ecdl/EcdlApplication.java
@@ -0,0 +1,12 @@
+package com.duskol.ecdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcdlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcdlApplication.class, args);
+    }
+}
