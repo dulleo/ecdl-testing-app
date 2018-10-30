702ea38ff2ef6f2fe4339e126968ee04cbc8165b
duskol <dulleo@gmail.com>
2018-10-30 01:40:28 +0100

    comment added

--- src/main/java/com/duskol/ecdl/entity/AppUser.java
+++ src/main/java/com/duskol/ecdl/entity/AppUser.java
@@ -4,6 +4,9 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.List;
 
+/**
+ * AppUser entity class
+ */
 @Entity(name = "user")
 public class AppUser {
 
